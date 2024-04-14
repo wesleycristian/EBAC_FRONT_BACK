ff7c239abd6c72b8da537c927305d8f6a92c1de1
wesleycristian <soldadocristiam@hotmail.com>
2024-04-13 23:50:54 -0300

    finalizado

--- FRONT END/exercicio_css_html/jQuery_ css _html/main.js
+++ FRONT END/exercicio_css_html/jQuery_ css _html/main.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
- 
+    $(document).ready(function() {
     $('header button').click (function() {
         $('form').slideDown();
     })
@@ -7,19 +6,19 @@ $(document).ready(function() {
     $('#botao-cancelar').click(function (){
 $ ('form').slideUp();
     })
-
+    
     $('form').on ('submit', function(e) {
         e.preventDefault();
         const enderecoDaNovaImagem = $ ('#endereco-imagem-nova').val();
-        const novoItem =$('<li></li>');
+        const novoItem =$('<li style= "display: none"></li>');
         $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem)
-        $(`
-        <div class="overlay-imagem-link">
-            <a href= "${enderecoDaNovaImagem}" target="_blank" title= "Ver imagem em tamnho real"
+        $(`<div class="over-lay-imagem-link">
+            <a href= "${enderecoDaNovaImagem}" target="_blanck" title= "Ver imagem em tamnho real">
                 Ver imagem tamanho real
             </a>
-        </div>
-        `).appendTo(novoItem)
-        &(novoItem).appendTo('ul');
+        </div>`).appendTo(novoItem)
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(2000)
+        $('#endereco-imagem-nova').val ('')
     }) 
 })
